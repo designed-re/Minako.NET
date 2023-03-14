ac7fe7818a4ce67597bbc4fc8237536b684a8709
SeojunYun <arfoneajp@gmail.com>
2023-03-14 15:18:34 +0900

    inital commit

---
+++ Minako.NET/Minako.cs
@@ -0,0 +1,19 @@
+﻿namespace Minako.NET
+{
+    public class Minako
+    {
+        public HttpClient HttpClient { get; }
+
+        public Minako()
+        {
+            HttpClient = new();
+            HttpClient.BaseAddress = new Uri("https://api.minako.moe");
+            
+        }
+
+        public Minako(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+    }
+}
